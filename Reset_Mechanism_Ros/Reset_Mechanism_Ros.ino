bcdd92ab86d3bd09e513f3307d2728d0bf0a8e98
charlie <charlie-kenny@hotmail.com>
2022-01-06 16:26:49 -0500

    Continued writing the reset_mechanism.ino file.  Added the publish data method, PID control for the turntable motors, finished homing method, and added timer ISR

--- Reset_Mechanism_Ros/Reset_Mechanism_Ros.ino
+++ Reset_Mechanism_Ros/Reset_Mechanism_Ros.ino
@@ -6,6 +6,14 @@
 #include <std_msgs/Bool.h>
 #include <infrastructure_msgs/Drawer.h>
 
+#define USE_TIMER_1     true
+#define USE_TIMER_2     false
+#define USE_TIMER_3     false
+#define USE_TIMER_4     false
+#define USE_TIMER_5     false
+
+#include <TimerInterrupt.h>
+
 // Stepper Motor Pins
 #define LINEAR_ACTUATOR_STEPPER_PULSE_PIN 0
 #define LINEAR_ACTUATOR_STEPPER_DIRECTION_PIN 0
@@ -41,6 +49,13 @@
 // Photo Interrupt Pin
 #define PHOTO_INTERRUPT_PIN A0
 
+// PID Coefficients
+#define Kp 1
+#define Kd 0.5
+
+// Turntable Rotation Tolerance
+#define TURNTABLE_TOLERANCE 2
+
 // Init the stepper motors
 AccelStepper linear_actuator_motor( AccelStepper::DRIVER, LINEAR_ACTUATOR_STEPPER_PULSE_PIN, LINEAR_ACTUATOR_STEPPER_DIRECTION_PIN);
 AccelStepper spool_motor( AccelStepper::DRIVER, SPOOL_STEPPER_PULSE_PIN, SPOOL_STEPPER_DIRECTION_PIN);
@@ -48,9 +63,15 @@ AccelStepper z_axis_motor( AccelStepper::DRIVER, Z_AXIS_STEPPER_PULSE_PIN, Z_AXI
 AccelStepper x_axis_motor( AccelStepper::DRIVER, X_AXIS_STEPPER_PULSE_PIN, X_AXIS_STEPPER_DIRECTION_PIN);
 AccelStepper arm_motor( AccelStepper::DRIVER, ARM_STEPPER_PULSE_PIN, ARM_STEPPER_DIRECTION_PIN);
 
+// PID Controller Setup
+PID_v2 PIDController(Kp, 0, Kd, PID::Direct);
+
 // current encoder reading variable
 volatile long encoder_reading = 0;
 volatile bool turntable_clockwise = true;
+
+// Current object on the reset mechanism
+uint8_t current_object;
 
 //ros variables
 ros::NodeHandle n;
@@ -138,6 +159,13 @@ void setup()
   x_axis_motor.setMaxSpeed(800);
   arm_motor.setMaxSpeed(800);
 
+  //configure the acceleration rates of the stepper motors
+  linear_actuator_motor.setAcceleration(200);
+  spool_motor.setAcceleration(200);
+  z_axis_motor.setAcceleration(200);
+  x_axis_motor.setAcceleration(200);
+  arm_motor.setAcceleration(200);
+
   //setup the enable pins on the motors
   linear_actuator_motor.setEnablePin(LINEAR_ACTUATOR_STEPPER_ENABLE_PIN);
   spool_motor.setEnablePin(SPOOL_STEPPER_ENABLE_PIN);
@@ -150,6 +178,10 @@ void setup()
   
 }
 
+
+// ISRs
+
+//Encoder Pin
 void update_encoder()
 {
   if(turntable_clockwise)
@@ -161,6 +193,18 @@ void update_encoder()
     encoder_reading--;
   }
 }
+
+//Timer
+void TimerHandler()
+{
+  linear_actuator_motor.run();
+  spool_motor.run();
+  z_axis_motor.run();
+  x_axis_motor.run();
+  
+}
+
+#define TIMER_INTERVAL_MS        1L
 
 void loop()
 {
@@ -176,17 +220,91 @@ void loop()
 void home_motors()
 {
   //Log that motors are being homed
-  n.loginfo("Starting drawer reset");
+  n.loginfo("Homing carriage and linear actuator motors");
 
   //change action status publisher
   action_status.data = true;
   statuspub.publish(&action_status);
 
-  bool motors mov
-  while(
+  bool x_motor_moving = true;
+  bool z_motor_moving = true;
+  bool linear_actuator_moving = true;
 
+  //Set the stepper motor speeds for the motors that need to be homed
+  linear_actuator_motor.setSpeed(-200);
+  z_axis_motor.setSpeed(-200);
+  x_axis_motor.setSpeed(-200);
+
+  //Loop while any of the motors that are being homed are moving
+  while(linear_actuator_moving || x_motor_moving || z_motor_moving)
+  {
+    //Check if the motor limit switches have been triggered
+    if(linear_actuator_moving)
+    {
+      if(get_linear_actuator_switch())
+      {
+        //Stop the motors if the limit switches are triggered
+        linear_actuator_motor.setSpeed(0);
+        linear_actuator_moving = false;
+      }
+    }
+    
+    if(z_motor_moving)
+    {
+      if(get_z_axis_switch())
+      {
+        z_axis_motor.setSpeed(0);
+        z_motor_moving = false;
+      }
+    }
+
+    if(x_motor_moving)
+    {
+      if(get_x_axis_switch())
+      {
+        x_axis_motor.setSpeed(0);
+        x_motor_moving = false;
+      }
+    }
+  }
+
+  //The motors are now in their home positions so reset the current positions on the stepper motors
+  linear_actuator_motor.setCurrentPosition(0);
+  z_axis_motor.setCurrentPosition(0);
+  x_axis_motor.setCurrentPosition(0);
+
+  //Log that motors are finished being homed
+  n.loginfo("Finished homing carriage and linear actuator motors");
+
+  //Move the carriage and linear actuator to a good setup location
+  // TO DETERMINE
   
 }
+
+
+void reset_current_object()
+{
+  
+}
+
+
+void swap_current_object(uint8_t new_object)
+{
+  n.loginfo("Swapping current object on the reset mechansim");
+  
+  // Check if the new object is already the object on the reset mechanism
+  if( new_object == current_object )
+  {
+    n.loginfo("The desired object is already on the reset mechansim. Stopping swap algorithm");
+    return;
+  }
+
+  int z_position_of_new_object;
+  int x_position_of_new_object;
+  
+  // Determine where the carraige needs to travel to
+}
+
 
 bool get_linear_actuator_switch()
 {
@@ -203,14 +321,62 @@ bool get_x_axis_switch()
   return !digitalRead(X_AXIS_SWITCH_PIN);
 }
 
-void set_turntable_motor_1(bool clockwise, uint8_t motor_speed)
+void publish_sensor_data()
 {
-  digitalWrite(TURNTABLE_MOTOR_1_DIRECTION_PIN, clockwise);
-  analogWrite(TURNTABLE_MOTOR_1_ENABLE_PIN, motor_speed);
+  data.turntable_position = encoder_reading;
+  
+  data.linear_actuator_position = linear_actuator_motor.currentPosition();
+  data.spool_position = spool_motor.currentPosition();
+  data.x_axis_position = z_axis_motor.currentPosition();
+  data.z_axis_position = x_axis_motor.currentPosition();
+  data.arm_position = arm_motor.currentPosition();
+
+  data.linear_actuator_switch = get_linear_actuator_switch();
+  data.z_axis_switch = get_z_axis_switch();
+  data.x_axis_actuator_switch = get_x_axis_switch();
+
+  data.current_object = current_object;
+  
+  data.header.stamp = n.now(); //gets the time of the device roscore is running on
+  datapub.publish(&data);
 }
 
-void set_turntable_motor_2(bool clockwise, uint8_t motor_speed)
+void move_turntable_to_position(int desired_position)
 {
+  //PID Initialization
+  PIDController.Start(encoder_reading,        // input
+                      0,                      // current output
+                      desired_position);      // setpoint
+
+  // Run the motors while the position of the turntable is not close to the desired position
+  while( abs(encoder_reading - desired_position) < TURNTABLE_TOLERANCE) )
+  {
+    int output = PIDController.Run(encoder_reading);
+    if( desired_position < encoder_reading )
+    {
+      set_turntable_motors(false, output);
+    }
+    else
+    {
+      set_turntable_motors(true, output);
+    }
+  }
+
+  //Once the turntable has rotated to the desired position, ensure that the motors have stopped moving
+  set_turntable_motors(true, 0);
+  
+}
+
+void set_turntable_motors(bool clockwise, uint8_t motor_speed)
+{
+  //Store the current direction the motor is turning so that the encoder can count in the right direction
+  turntable_clockwise = clockwise;
+
+  //Set the voltages on the motor1 output pins to set the desired motor output
+  digitalWrite(TURNTABLE_MOTOR_1_DIRECTION_PIN, clockwise);
+  analogWrite(TURNTABLE_MOTOR_1_ENABLE_PIN, motor_speed);
+
+  //Set the voltages on the motor2 output pins to set the desired motor output
   digitalWrite(TURNTABLE_MOTOR_2_DIRECTION_PIN, clockwise);
   analogWrite(TURNTABLE_MOTOR_2_ENABLE_PIN, motor_speed);
 }
